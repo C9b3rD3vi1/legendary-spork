df974b840fdb9844827ba04a20c928b7e34ce129
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-04-11 20:11:23 +0300

    conditions and operators

---
+++ loopscode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Demonstration of for conditions and variable assignment in bash
+
+#Comparison Operators:
+
+#-eq (equal), -ne (not equal)
+
+#-lt (less than), -gt (greater than)
+
+#-z (string is empty)
+echo "Comparison Operators:"
+echo "---------------------"
+
+number=1
+echo "Number: $number"
+
+read -p "Enter Age: " age  # Prompt user for input
+
+# if statement
+if [ $age -lt 18 ]; then
+    echo "You are a minor."
+else
+    echo "You are an adult."
+fi
