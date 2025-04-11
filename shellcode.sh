6641f77b6261d136c0163409651c5cff6e9b0e3c
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-04-11 19:47:33 +0300

    bash variables

---
+++ shellcode.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# A simple shell script to demonstrate the use of variables and loops
+
+# Define a variable
+greeting="Hello, World!"
+echo $greeting
+
+#variable assignment
+name="C9b3rD3vi1"
+role="Hacker"
+age=30
+
+# Print the variables
+echo "Name: $name"
+echo "Role: $role"
