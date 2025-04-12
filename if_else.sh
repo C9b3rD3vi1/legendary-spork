3ab139943e07ae1df94e16321c81cb4d0e8db018
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-04-12 16:57:57 +0300

    rando number using if_else

---
+++ if_else.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script demonstrates the use of if-else statements in bash
+
+echo "If-Else Statements in Bash"
+echo "---------------------"
+
+# Allow user to enter a number
+read -p "Enter a number: " num  # Prompt user for input
+
+random_number=$((RANDOM % 10 + 1))  # Generate a random number between 1 and 10
+# Check if the entered number is equal to the random number
+if [ $num -eq $random_number ]; then
+    echo "Congratulations! You guessed the number."
+else
+    echo "Sorry, the correct number was $random_number."
+fi
